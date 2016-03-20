8cabbc57d393dec6e62a4ba134e392ece3ebc0ae
Adithya Abraham Philip <adithyaphilip@gmail.com>
2016-03-20 18:00:42 +0530

    anjana's updated code

--- app/src/main/java/com/example/anjana/pescom/LoginActivity.java
+++ app/src/main/java/com/example/anjana/pescom/LoginActivity.java
@@ -1,45 +1,36 @@
 package com.example.anjana.pescom;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLEncoder;
-
+import java.util.Scanner;
 
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_OTP = 0;
-    private EditText _phoneText;
-    private Button _loginButton;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-    //private TextView _signupLink;
+    protected static EditText _phoneText;
+    protected static Button _loginButton;
+    protected static TextView _error;
+    private ProgressDialog progressDialog;
+
+    public boolean success;
 
 
     @Override
@@ -50,186 +41,49 @@ public class LoginActivity extends AppCompatActivity {
 
         _loginButton = (Button) findViewById(R.id.btn_login);
 
+        _error = (TextView) findViewById(R.id.error);
+
 
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                sendRequest_phone();//send post request
-                Intent intent = new Intent(getApplicationContext(), OTPActivity.class);
-                startActivityForResult(intent, REQUEST_OTP);
-                onLoginSuccess();
+                _error.setText("");
+                Log.i("FUNC", "loginbuttonClickedINPage1");
+                //send post request
+                if(validate())
+                    sendRequest_phone();
+                else
+                {
+                    Toast.makeText(getBaseContext(), "INVALID PHONE NUMBER", Toast.LENGTH_LONG).show();
+                }
+                Log.i("return:", "after everything");
+
             }
         });
 
+        progressDialog = new ProgressDialog(LoginActivity.this, R.style.AppTheme_Dark_Dialog);
 
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void sendRequest_phone()
-    {
+    public String sendRequest_phone() {
 
         String phone = _phoneText.getText().toString();
-        String data = null;
+        _error.setText("");
+        Log.i("FUNC", "sendRequest_phone");
         try {
-            data = URLEncoder.encode("name", "UTF-8")+"="+ URLEncoder.encode(phone,"UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+
+            progressDialog.setMessage("Authenticating..");
+            progressDialog.show();
+            new MyTask().execute(phone);
+        } catch (Exception E) {
+            E.printStackTrace();
         }
-        URL url = null;//change name
-        try {
-            url = new URL("www.google.com");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        HttpURLConnection conn= null;
-        try {
-            conn = (HttpURLConnection)url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try
-        {
-            //sending request for otp
-            conn.setDoOutput(true);
-            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
-            wr.write( data );
-            wr.flush();
-
-            int status = conn.getResponseCode();
-            if (status != 200) {
-                throw new IOException("Post failed with error code " + status);
-            }
-        }
-        catch(IOException e)
-        {
-            e.printStackTrace();
-        }
-
-        //receive response
-        InputStream res= null;
-        try
-        {
-              res = conn.getInputStream();
-        }
-        catch (IOException e)
-        {
-                e.printStackTrace();
-        }
-
-        String response=convertStreamToString(res);
-
-        JSONObject json= null;
-        try
-        {
-            json = new JSONObject(response);
-            if(json.get("success").equals("true"))
-            {
-                //receive the otp ---> askshay sowmayaji
-            }
-            else
-            {
-                //display message as otp was not received
-
-
-            }
-        }
-        catch (JSONException e)
-        {
-            e.printStackTrace();
-        }
-
-
-
-
+        return null;
 
 
     }
-
-    private static String convertStreamToString(InputStream is) {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-
-        String line ="";
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
-
-    /*public void otp() {
-        Log.d(TAG, "Login");
-
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        _loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-
-        new Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-
-    }
-
-    public void login() {
-        Log.d(TAG, "Login");
-
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        _loginButton.setEnabled(false);
-
-        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
-
-        String phone = _phoneText.getText().toString();
-        // String password = _passwordText.getText().toString();
-
-        // TODO: Implement your own authentication logic here.
-
-        new Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }*/
 
 
     @Override
@@ -238,7 +92,7 @@ public class LoginActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
 
                 // TODO: Implement successful signup logic here
-                // By default we just finish the Activity and log them in automatically
+
                 this.finish();
             }
         }
@@ -250,13 +104,12 @@ public class LoginActivity extends AppCompatActivity {
         moveTaskToBack(true);
     }
 
-    public void onLoginSuccess() {
+    public static void onLoginSuccess() {
         _loginButton.setEnabled(true);
-        finish();
     }
 
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+    public void onLoginFailed(String s) {
+        Toast.makeText(getBaseContext(), s, Toast.LENGTH_LONG).show();
 
         _loginButton.setEnabled(true);
     }
@@ -267,82 +120,129 @@ public class LoginActivity extends AppCompatActivity {
         boolean valid = true;
 
         String phone = _phoneText.getText().toString();
-        //String password = _passwordText.getText().toString();
 
-        char plus = phone.charAt(0);
-        if (plus == '+') {
-            System.out.print("aaaa");
-            if (phone.length() != 15 || phone.length() != 13) {
-                System.out.print("abbb");
-                valid = false;
-            }
-
-        } else if (phone.length() != 10) {
-            valid = false;
-            System.out.print("hfjjdna");
-        }
-
-
-        //||!android.util.Patterns.PHONE.matcher(phone).matches()
-        if (phone.isEmpty() || !valid) {
-            _phoneText.setError("Please enter correct phone number");
-            valid = false;
-
-        } else {
-            _phoneText.setError(null);
-        }
-
-
-        /*if (password.isEmpty() || password.length() < 4 || password.length() > 10)
-        {
-            _passwordText.setError("Between 4 and 10 alphanumeric characters");
-            valid = false;
-        }
+        if(phone.length()!=10)
+            valid=false;
         else
         {
-            _passwordText.setError(null);
-        }*/
+            if(!phone.matches("[0-9]+"))
+            {
+                valid=false;
+            }
+
+        }
 
         return valid;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Login Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.anjana.pescom/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+    private class MyTask extends AsyncTask<String, Boolean, Boolean> {
+
+        private String error_str="";
+
+        @Override
+        protected void onPreExecute() {
+
+
+        }
+
+        protected Boolean doInBackground(String... urls) {
+
+            try {
+
+                    String data = "phone_number=" + urls[0].toString();
+                    URL url = new URL("https://secure-garden-80717.herokuapp.com/signup");
+                    Log.i("  EventLogin", "openURL");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    Log.i("  EventLogin", "OpeningURLConnection");
+                    conn.setRequestMethod("POST");
+                    conn.setDoOutput(true);
+
+                    DataOutputStream dStream = new DataOutputStream(conn.getOutputStream());
+                    dStream.writeBytes(data);
+                    dStream.flush();
+
+                    int status = conn.getResponseCode();
+                    Log.i("  Status", String.valueOf(status));
+                    if (status != 200) {
+                        throw new IOException("Post failed with error code " + status);
+                    }
+
+
+                    Log.i("  EventLogin", "sentOTPRequest");
+
+                    InputStream res = conn.getInputStream();
+                    Log.i("  EventLogin", "Receiving Response");
+
+                    String response = convertStreamToString(res);
+                    Log.i("Response:", response);
+                    JSONObject json = new JSONObject(response);
+
+                    if (json.get("success").toString().equals("true")) {
+                        //receive the otp ---> askshay sowmyaji
+                    } else {
+                        //display message as otp was not received
+
+                        error_str="OTP not received. Login failed";
+                        return false;
+
+                    }
+
+            } catch (Exception E) {
+
+                E.printStackTrace();
+                Log.i("errorLOGIN", "Exception");
+
+                error_str="Login failed";
+                return false;
+
+
+            }
+
+            return true;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean s) {
+            progressDialog.dismiss();
+            if (s) {
+                Intent intent = new Intent(getApplicationContext(), OTPActivity.class);
+                intent.putExtra("phone", _phoneText.getText().toString());
+
+                startActivityForResult(intent, REQUEST_OTP);
+                onLoginSuccess();
+                LoginActivity.this.finish();
+            } else {
+                Log.i("elsePostExecute", "failed");
+                onLoginFailed(error_str);
+            }
+        }
+
+
+        private String convertStreamToString(InputStream is) {
+
+
+            StringBuilder sb = new StringBuilder();
+            Scanner se = new Scanner(is);
+
+            String line = "";
+            try {
+                while (se.hasNextLine()) {
+                    sb.append(se.nextLine() + "\n");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sb.toString();
+        }
+
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Login Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://com.example.anjana.pescom/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
--- app/src/main/java/com/example/anjana/pescom/OTPActivity.java
+++ app/src/main/java/com/example/anjana/pescom/OTPActivity.java
@@ -1,21 +1,52 @@
 package com.example.anjana.pescom;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
 
 public class OTPActivity extends AppCompatActivity {
 
-    private Button _loginButton;
+    protected Button _loginButton;
+    protected EditText _OTPText;
+    protected static TextView _error;
+    protected String phone;
+    protected static String storage = "User_tokens";
+    protected ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
-        _loginButton=(Button)findViewById(R.id.btn_login);
+
+
+        Intent intent = getIntent();
+
+        // 2. get message value from intent
+        phone = intent.getStringExtra("phone");
+
+        _loginButton = (Button) findViewById(R.id.btn_login);
+        _OTPText = (EditText) findViewById(R.id.input_otp);
+        _error = (TextView) findViewById(R.id.error);
+
         _loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -24,56 +55,166 @@ public class OTPActivity extends AppCompatActivity {
             }
         });
 
+        progressDialog = new ProgressDialog(OTPActivity.this, R.style.AppTheme_Dark_Dialog);
+
     }
 
 
     public void login() {
-       // Log.d(TAG, "Login");
-
-
 
         _loginButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(OTPActivity.this,
-                R.style.AppTheme_Dark_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Authenticating...");
-        progressDialog.show();
+        _error.setText("");
+        Log.i("EventOTP:", "INLOgin");
 
-        //String phone = _phoneText.getText().toString();
-        // String password = _passwordText.getText().toString();
+        String otp = _OTPText.getText().toString();
 
-        // TODO: Implement your own authentication logic here.
 
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onLoginSuccess or onLoginFailed
-                        onLoginSuccess();
-                        // onLoginFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        try {
+
+            progressDialog.setMessage("Authenticating..");
+            progressDialog.show();
+            if (!otp.equals("")) {
+
+                new Task().execute(phone, otp);
+            }
+            else {
+               onLoginFailed("Please enter OTP");
+            }
+
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
+
+    }
+
+    public void storeData(JSONObject json, String phone) {
+        SharedPreferences mem = getSharedPreferences(storage, MODE_PRIVATE);
+        try {
+            mem.edit().putString(phone, json.get("token").toString());
+        } catch (Exception E) {
+            E.printStackTrace();
+        }
     }
 
 
-
-    @Override
+   @Override
     public void onBackPressed() {
         // Disable going back to the MainActivity
         moveTaskToBack(true);
     }
 
     public void onLoginSuccess() {
+        Toast.makeText(getBaseContext(), "LOGIN SUCCESSFUL", Toast.LENGTH_LONG).show();
         _loginButton.setEnabled(true);
         finish();
     }
 
-    public void onLoginFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+    public void onLoginFailed(String s) {
+        Toast.makeText(getBaseContext(), s, Toast.LENGTH_LONG).show();
 
         _loginButton.setEnabled(true);
+
     }
 
+
+    public static void setErrorText(String s) {
+        _error.setText(s);
+    }
+
+
+    private class Task extends AsyncTask<String, Void, Boolean> {
+
+        private String error_str="";
+
+        protected Boolean doInBackground(String... urls) {
+
+            String data = null;
+            try {
+                Log.i("EventOTP:", "INTASK");
+                //data = URLEncoder.encode("phone_number", "UTF-8") + "=" + URLEncoder.encode(urls[0], "UTF-8");
+                //data += "&" + URLEncoder.encode("otp", "UTF-8") + "=" + URLEncoder.encode(urls[1], "UTF-8");
+                data="phone_number="+ URLEncoder.encode(urls[0], "UTF-8")+"&otp="+ URLEncoder.encode(urls[1], "UTF-8");
+                URL url = new URL("https://secure-garden-80717.herokuapp.com/authenticate");
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoOutput(true);
+
+
+                System.out.println(data);
+                DataOutputStream dStream = new DataOutputStream(conn.getOutputStream());
+                dStream.writeBytes(data);
+                dStream.flush();
+
+                int status = conn.getResponseCode();
+                if (status != 200) {
+                    throw new IOException("Post failed with error code " + status);
+                }
+                InputStream res = conn.getInputStream();
+                String response = convertStreamToString(res);
+
+                JSONObject json = new JSONObject(response);
+
+                if (json.get("success").toString().equals("true")) {
+                    //save token and ph number
+                    OTPActivity.this.storeData(json, urls[0]);
+
+
+                } else {
+
+                    error_str="Login failed";
+                    return false;
+
+                }
+            } catch (Exception E) {
+
+                E.printStackTrace();
+                error_str="Login failed";
+                return false;
+
+
+            }
+            return true; //change this
+        }
+
+        @Override
+        protected void onPostExecute(Boolean s) {
+            progressDialog.dismiss();
+            if (s) {
+
+
+                //Create new activity for user account
+                onLoginSuccess();
+                OTPActivity.this.finish();
+            } else {
+                Log.i("elsePostExecute", "failed");
+                onLoginFailed(error_str);
+            }
+        }
+
+
+        private String convertStreamToString(InputStream is) {
+
+            //BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            Scanner se = new Scanner(is);
+
+            String line = "";
+            try {
+                while (se.hasNextLine()) {
+                    sb.append(se.nextLine() + "\n");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return sb.toString();
+        }
+    }
 
 }
